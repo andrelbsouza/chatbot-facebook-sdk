1167f6117a90dc8f783ed8ff004e66a13ad10caa
Andre Souza <func068@cecresld.com.br>
2018-11-22 16:46:33 -0200

    Criado teste de integração e classe de requisição

---
+++ src/CallSendApi.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: func068
+ * Date: 22/11/2018
+ * Time: 12:42
+ */
+
+namespace ChatBot;
+
+
+class CallSendApi
+{
+
+}
---
+++ tests/integration/CallSendApiTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: func068
+ * Date: 22/11/2018
+ * Time: 16:31
+ */
+
+class CallSendApiTest
+{
+
+}
