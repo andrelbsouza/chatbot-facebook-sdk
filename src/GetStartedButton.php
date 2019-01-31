19d3a2eb2fb83833af9035fbef9cb8a9d07ad9b5
André Souza <andre.b.souza@gmail.com>
2019-01-31 15:45:00 -0200

    Implementação da classe para adicionar o botão começar

---
+++ src/GetStartedButton.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: func068
+ * Date: 31/01/2019
+ * Time: 14:47
+ */
+
+namespace ChatBot;
+
+
+class GetStartedButton
+{
+
+}
---
+++ tests/integration/GetStartedButtonTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace ChatBot;
+
+use ChatBot\Message\Text;
+use PHPUnit\Framework\TestCase;
+
+class CallSendApiTest extends TestCase
+{
+    /**
+     * @expectedException  \GuzzleHttp\Exception\ClientException
+     */
+    public function testMakeRequest(){
+        $message = (new Text(1))->message('Oiii');
+        (new CallSendApi('28sj82'))->make($message);
+    }
+}
